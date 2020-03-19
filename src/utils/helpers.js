b8c23af2d133a48c214ac6759f566d4c4da6461c
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-19 21:17:30 +0100

    modified e-mail validation logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken')
 const { BadRequestError } = require('./http-errors')
 
 module.exports = {
-    generateToken: (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h', subject: 'TeamWork-nodeJS' }),
+    generateToken: (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h', subject: 'Cust0merCare-nodeJS' }),
 
-    isValidEmail: (email) => /^([a-zA-Z0-9_\-]+)(\.)?([a-zA-Z0-9_\-]+)@([a-zA-Z]+)\.([a-zA-Z]{2,})$/.test(email),
+    isValidEmail: (email) => (!email && /^([a-zA-Z0-9_\-]+)(\.)?([a-zA-Z0-9_\-]+)@([a-zA-Z]+)\.([a-zA-Z]{2,})$/.test(email)),
 
     validateParameters: (inputParams, paramsToVerify) => {
         Object.entries(inputParams).forEach(([key, value]) => {
