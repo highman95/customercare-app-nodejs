36ad4d63d57bbb6ef3cf312e1f78f96c14ee56f6
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-22 21:23:26 +0100

    added account de-/activation route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,9 @@
 const userController = require('../controllers/user');
+const { authWare } = require('../utils/middlewares');
 
 module.exports = (router) => {
     router.post('/users/signin', userController.authenticate);
     router.post('/users/register', userController.create);
+    router.put('/users/activate', authWare, userController.activate);
+    router.put('/users/deactivate', authWare, userController.deactivate);
 }
