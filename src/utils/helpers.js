4bf3e0a862ebef59042d0c8a913e523d66ee8d60
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-22 11:46:42 +0100

    minor bug-fix on email-validation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,7 +4,7 @@ const { BadRequestError } = require('./http-errors');
 module.exports = {
     generateToken: (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: `${process.env.JWT_ELAPSE}h`, subject: 'Cu$t0merCar£-nodeJS' }),
 
-    isValidEmail: (email) => (!email && /^([a-zA-Z0-9_\-]+)(\.)?([a-zA-Z0-9_\-]+)@([a-zA-Z]+)\.([a-zA-Z]{2,})$/.test(email)),
+    isValidEmail: (email) => (!!email && /^([a-zA-Z0-9_\-]+)(\.)?([a-zA-Z0-9_\-]+)@([a-zA-Z]+)\.([a-zA-Z]{2,})$/.test(email)),
 
     validateParameters: (inputParams, paramsToVerify) => {
         Object.entries(inputParams).forEach(([key, value]) => {
