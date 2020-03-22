cc74dc57b287a2a9b96cb0efff657b378de91cb3
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-22 11:24:14 +0100

    added user-registration route start #user-registration

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,4 +2,5 @@ const userController = require('../controllers/user');
 
 module.exports = (router) => {
     router.post('/users/signin', userController.authenticate);
+    router.post('/users/register', userController.create);
 }
