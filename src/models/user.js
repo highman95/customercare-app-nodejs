560d8f2b42480c8bfbf378b8103fd9dbaa56ac01
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-22 11:56:52 +0100

    added user-creation model logic

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
-const { dbEntities, isValidEmail } = require('../utils/helpers');
-const { BadRequestError, UnauthorizedError } = require('../utils/http-errors');
+const bcrypt = require('bcrypt');
+const { dbEntities, isValidEmail, validateParameters } = require('../utils/helpers');
+const { BadRequestError, ConflictError, NotAcceptableError, UnauthorizedError } = require('../utils/http-errors');
 
 module.exports = {
     async authenticate(username, password) {
@@ -14,6 +15,33 @@ module.exports = {
             return user;
         } catch (e) {
             throw new UnauthorizedError('Invalid username / password');
+        }
+    },
+
+    async create(firstName, lastName, gender, address, email, password) {
+        const params = ['firstName', 'lastName', 'gender', 'email', 'password', 'address'];
+        const submittedInput = { firstName, lastName, gender, email, password, address };
+        validateParameters(submittedInput, params);
+
+        const genderLcase = gender.toLowerCase();
+        if (!['male', 'female'].includes(genderLcase)) throw new NotAcceptableError('The gender can only be male or female');
+
+        if (!!await this.findByEmail(email)) throw new ConflictError('E-mail address already exists');
+
+        //encrypt the password and compute input parameters
+        let hashedPassword;
+        try {
+            hashedPassword = await bcrypt.hash(password, 10);
+        } catch (e) {
+            throw new Error('The password could not be encrypted');
+        }
+
+        try {
+            const input = [firstName, lastName, genderLcase, email, hashedPassword, address, 2];
+            const result = await db.query(`INSERT INTO ${dbEntities.users} (first_name, last_name, gender, email, password, address, cadre_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, first_name, email`, input)
+            return (result.rowCount === 0) ? null : result.rows[0];
+        } catch (e) {
+            throw new DatabaseError('User could not be saved');
         }
     },
 
