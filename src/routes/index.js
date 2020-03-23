878bd7c1b364513f85bc684093fab110726724b7
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-23 11:36:51 +0100

    added item-route aggregator

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./user');
 const categoryRoutes = require('./category');
+const itemRoutes = require('./item');
 
 module.exports = (router) => {
     userRoutes(router);
     categoryRoutes(router);
+    itemRoutes(router);
 
     // set a default PING / Health-Check route
     router.get('/ping', (req, res) => res.json({ status: 'success', error: 'connected...pong...pong...pong...' }));
