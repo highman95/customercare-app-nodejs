a3e1997f4e3739d95c57609bb655164365cae755
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-23 08:54:19 +0100

    added route for fetch-categories

--- src/routes/category.js
+++ src/routes/category.js
@@ -3,4 +3,5 @@ const { authWare } = require('../utils/middlewares');
 
 module.exports = (route) => {
     route.post('/categories', authWare, controller.create);
+    route.get('/categories', controller.fetch);
 }
