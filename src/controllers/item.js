770a42a57388c049009faa626c5c485dc3c6e85d
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-23 12:18:03 +0100

    added create-product request-handler

---
+++ src/controllers/item.js
@@ -0,0 +1,14 @@
+const model = require('../models/item');
+
+module.exports = {
+    create: async (req, res, next) => {
+        const { body: { name, price, requirements, category_id } } = req;
+
+        try {
+            const item = await model.create(category_id, name, price, requirements);
+            res.status(201).json({ status: 'success', data: { message: 'Category-item created successfully', item } });
+        } catch (e) {
+            next(e)
+        }
+    }
+}
