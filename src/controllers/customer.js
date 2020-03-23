dbf245981b74f20223293701297e2632976ddd85
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-23 22:07:00 +0100

    added requester-handler for create-customer

---
+++ src/controllers/customer.js
@@ -0,0 +1,14 @@
+const model = require('../models/customer');
+
+module.exports = {
+    create: async (req, res, next) => {
+        const { body: { first_name, last_name, birth_date, gender, email, phone }, user } = req;
+
+        try {
+            const customer = await model.create(first_name, last_name, birth_date, gender, email, phone, user.id);
+            res.status(201).json({ status: 'success', data: { customer, message: 'Customer created successfully' } });
+        } catch (e) {
+            next(e)
+        }
+    }
+}
