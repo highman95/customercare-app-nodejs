fa619937bda4c6ef90e13c29b3725bb2ba91d637
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-23 10:27:32 +0100

    added fetch-categories request-handler

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,12 +1,23 @@
-const categoryModel = require('../models/category');
+const model = require('../models/category');
 
 module.exports = {
     create: async (req, res, next) => {
-        const { body: { name }, user } = req;        
+        const { body: { name }, user } = req;
 
         try {
-            const category = await categoryModel.create(name, user.id);
+            const category = await model.create(name, user.id);
             res.status(201).json({ status: 'success', data: { message: 'Category created successfully', category } });
+        } catch (e) {
+            next(e)
+        }
+    },
+
+    fetch: async (req, res, next) => {
+        const { q } = req.query;
+
+        try {
+            const categories = await model.fetchAll(q);
+            res.status(200).json({ status: 'success', data: categories });
         } catch (e) {
             next(e)
         }
