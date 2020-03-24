49a3c8c7f644c4eaae2d8b866f31aa37c6e9d7aa
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-25 00:16:07 +0100

    added bill-routes to route-aggregator

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const userRoutes = require('./user');
 const categoryRoutes = require('./category');
 const productRoutes = require('./product');
 const customerRoutes = require('./customer');
+const billRoutes = require('./bill');
 
 module.exports = (router) => {
     userRoutes(router);
     categoryRoutes(router);
     productRoutes(router);
     customerRoutes(router);
+    billRoutes(router);
 
     // set a default PING / Health-Check route
     router.get('/ping', (req, res) => res.json({ status: 'success', error: 'connected...pong...pong...pong...' }));
