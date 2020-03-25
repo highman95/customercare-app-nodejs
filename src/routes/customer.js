c883a547cc95777016a433d3738d8ea8a2083b35
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-25 23:06:43 +0100

    added fetch-customers route

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,4 +3,5 @@ const { authWare } = require('../utils/middlewares');
 
 module.exports = (router) => {
     router.post('/customers', authWare, controller.create);
+    router.get('/customers', authWare, controller.fetch);
 }
