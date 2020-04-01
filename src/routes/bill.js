50fac43a9a719db83b82f1b4c8ffcfa6a9d995f3
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-04-01 12:23:00 +0100

    added bill-status-update route

--- src/routes/bill.js
+++ src/routes/bill.js
@@ -5,4 +5,5 @@ module.exports = (router) => {
     router.post('/bills', authWare, controller.create);
     router.get('/bills', authWare, controller.fetch);
     router.get('/bills/:id', authWare, controller.find);
+    router.put('/bills/:id', authWare, controller.editStatus);
 };
