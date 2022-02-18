dd59956c42ee8fe19dea06a1204c5643156ddd38
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2022-02-18 16:05:42 +0100

    resolved minor bug + npm-audit issues

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const server = app.listen(process.env.PORT || 3000, (error) => {
-  console.log(error ? `Error: ${error.message}...` : `Listening on PORT: ${server.address().port}`);
-}).on('error', (error) => {
-  const report = (error.code === 'EADDRINUSE') ? `The port [${process.env.PORT || 3000}] is in use` : undefined;
-  console.log(report || `${error.name} --- ${error.message}`);// eslint-disable-line no-console
-});
+const server = app
+  .listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on PORT: ${server.address().port}`);
+  })
+  .on('error', (error) => {
+    const report = error.code === 'EADDRINUSE'
+      ? `The port [${process.env.PORT || 3000}] is in use`
+      : undefined;
+    console.log(report || `${error.name} --- ${error.message}`); // eslint-disable-line no-console
+  });
 
 module.exports = server;
